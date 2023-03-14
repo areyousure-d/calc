0cc1b04e067eccc1a1e3b09bbf177d5316de5b6d
areyousure <areyousure92@gmail.com>
2023-03-14 18:41:47 +0300

    add eslint and path aliases

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+const { configure, presets } = require("eslint-kit");
+
+module.exports = configure({
+  presets: [
+    presets.imports({
+      sort: {
+        newline: true,
+      },
+      alias: {
+        root: "./src",
+        "@": "./",
+      },
+    }),
+    presets.node(),
+    presets.prettier(),
+    presets.typescript({
+      root: "./",
+      tsconfig: "tsconfig.json",
+    }),
+    presets.react({
+      newJSXTransform: true,
+    }),
+  ],
+  extend: {
+    rules: {
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    },
+  },
+});
